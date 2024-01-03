767545c32efdcb12b8986e95ec75a33f5e9f917c
Jared Elliott <bootcamp@jaredclt.com>
2024-01-03 13:46:20 -0500

    Created links to show deals by each ID as proof of working.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,12 @@ function getStores() {
     // https://www.cheapshark.com/api/1.0/stores
     // Returns a full list of store IDs, names, and a flag specifying if store is active.
     // Response also includes an collection of banner / logo / icon image URLs for each store.
-    const apiCall = 'https://www.cheapshark.com/api/1.0/stores';
-    const baseURL = 'https://www.cheapshark.com'
 
+    // Default variables needed in fetch
+    const baseURL = 'https://www.cheapshark.com'
+    const apiCall = baseURL + '/api/1.0/stores';
+    const apiDeals = baseURL + '/api/1.0/deals'
+    
     var jqxhr = $.get(apiCall, function() {
         console.log("Fetch:", "Success.")
       })
@@ -21,19 +24,18 @@ function getStores() {
             //This is where the data is handled
             for(const key in data) { //Loops through each key in data array
                 //console.log("Feched:", data[key].storeID + " | " + data[key].storeName)
-                let storeIdentification = data[key].storeID + " | " + data[key].storeName
+                let storeID = data[key].storeID;
+                let storeName = data[key].storeName;
                 let storeActive = data[key].isActive;
-                let storeBanner = baseURL + data[key].images.banner;
-                let storeLogo = baseURL + data[key].images.logo;
-                let storeIcon = baseURL + data[key].images.icon;
-                let storeImages = storeBanner + " | " + storeLogo + " | " + storeIcon;
+                // Usage: storeImage.banner, storeImage.icon, storeImage.logo
+                let storeImage = data[key].images;
 
                 console.log()
-                //storeList.append('<p name="store">' + storeIdentification + '<br />' + storeImages + '</p>');
-                storeList.append('<p name="store">' + storeIdentification + '</p>' + '<img name="image" src="' + storeBanner + '">');
+                storeList.append('<p><a name="store" href="' + apiDeals + "?storeID=" + storeID + '">' +
+                storeName + '</a>' + '<img name="image" src="' + baseURL + storeImage.banner + '"></p>');
                 
                 if(storeActive == 1) { // if active is 1 (true), set class to active for styling
-                    $('p[name="store"]').addClass("active");
+                    $('a[name="store"]').addClass("active");
                 }
                 
             }
