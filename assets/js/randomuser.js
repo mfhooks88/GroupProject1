9ba2d879ca70f46817fb4d19bd87b8cda02d7559
Jared Elliott <bootcamp@jaredclt.com>
2024-01-07 10:25:58 -0500

    Created basic random user api.

---
+++ assets/js/randomuser.js
@@ -0,0 +1,22 @@
+
+
+function getRandomUser(gender, nationality, onlyReturn) {
+    const userAPI = 'https://randomuser.me/api/?results=1';
+    const noInfo = '&noinfo';
+
+    var jqxhr = $.get(userAPI + noInfo)
+    .done(function(data) {
+      //Loaded Information Successfully
+      console.log(data.results);
+    })
+    .fail(function() {
+      console.log("RandomUser:", "Failed.")
+    })
+    .always(function() {
+      console.log("RandomUser:", "Completed.")
+    });
+}
+
+
+
+getRandomUser('male','gb,us');
